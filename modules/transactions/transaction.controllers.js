dd4b8c49b5a2c8d70185e1e1d44ac732b9197e2d
Manohar Naidu <beesettim27@gmail.com>
2024-04-17 11:11:21 +0530

    updateTransaction API added

--- modules/transactions/transaction.controllers.js
+++ modules/transactions/transaction.controllers.js
@@ -73,3 +73,75 @@ exports.getTransactions = async (req, res) => {
 
   res.status(200).json({ transactions });
 };
+
+exports.deleteTransaction = async (req, res) => {
+  const { id } = req.params;
+  if (!id) throw new Error("Transaction ID is required");
+
+  const transaction = await Transaction.findById(id);
+  if (!transaction) throw new Error("Transaction not found");
+
+  await Transaction.findByIdAndDelete(id);
+
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $inc: {
+        balance:
+          transaction.transaction_type === "income"
+            ? -transaction.amount
+            : transaction.amount,
+      },
+    },
+    { runValidators: true }
+  );
+
+  res.status(200).json({
+    status: "success",
+    message: "Transaction deleted successfully",
+  });
+};
+
+exports.updateTransaction = async (req, res) => {
+  const { id } = req.params;
+  const { amount, remarks } = req.body;
+  if (!id) throw new Error("Transaction ID is required");
+  if (!amount) throw new Error("Amount is required");
+  if (!remarks) throw new Error("Remarks is required");
+  if (amount < 0) throw new Error("Amount cannot be negative");
+  if (typeof amount !== "number") throw new Error("Amount must be a number");
+  if (typeof remarks !== "string") throw new Error("Remarks must be a string");
+  if (amount === 0) throw new Error("Amount cannot be zero");
+  if (remarks.length < 3) throw new Error("Remarks is too short");
+  if (remarks.length > 100) throw new Error("Remarks is too long");
+
+  const transaction = await Transaction.findById(id);
+  if (!transaction) throw new Error("Transaction not found");
+
+  await Transaction.findByIdAndUpdate(
+    id,
+    {
+      amount,
+      remarks,
+    },
+    { runValidators: true }
+  );
+
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $inc: {
+        balance:
+          transaction.transaction_type === "income"
+            ? -transaction.amount + amount
+            : transaction.amount - amount,
+      },
+    },
+    { runValidators: true }
+  );
+
+  res.status(200).json({
+    status: "success",
+    message: "Transaction updated successfully",
+  });
+}
--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -5,10 +5,14 @@ const {
   addIncome,
   addExpense,
   getTransactions,
+  deleteTransaction,
+  updateTransaction,
 } = require("./transaction.controllers");
 
 router.get("/", auth, getTransactions);
 router.post("/addIncome", auth, addIncome);
 router.post("/addExpense", auth, addExpense);
+router.patch("/updateTransaction/:id", auth, updateTransaction);
+router.delete("/deleteTransaction/:id", auth, deleteTransaction);
 
 module.exports = router;
